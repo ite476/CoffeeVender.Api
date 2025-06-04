e56fff9dbcc9443023d080d43b4c660fde7226d8
성승현 <ite476@gmail.com>
2025-06-04 21:32:17 +0900

    fix: JTE 의존성 제거
    > HTML 필요 없음 (프론트용임)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("gg.jte.gradle") version "3.1.16"
 	kotlin("plugin.jpa") version "1.9.25"
 }
 
@@ -21,7 +20,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("gg.jte:jte-spring-boot-starter-3:3.1.16")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
@@ -37,11 +35,6 @@ java {
 	toolchain {
 		languageVersion.set(JavaLanguageVersion.of(21))
 	}
-}
-
-jte {
-	generate()
-	binaryStaticContent = true
 }
 
 allOpen {
