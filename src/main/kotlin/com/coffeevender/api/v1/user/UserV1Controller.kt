a9fb585f02efe7500ba89e0b97c392a09da9f466
성승현 <ite476@gmail.com>
2025-06-15 15:43:47 +0900

    add: UserV1Controller 에러케이스 정의 add: UserV1JoinRequest.kt

--- src/main/kotlin/com/coffeevender/api/v1/user/UserV1Controller.kt
+++ src/main/kotlin/com/coffeevender/api/v1/user/UserV1Controller.kt
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.*
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import com.coffeevender.api.v1.user.dto.request.UserV1JoinRequest
+import com.coffeevender.api.v1.common.dto.response.ApiResponseV1
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -16,20 +20,81 @@ class CoffeeUserV1Controller {
         description = "회원 가입하는 API",
         responses = [
             ApiResponse(responseCode = "201", description = "회원 가입 완료"),
-            ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            ApiResponse(responseCode = "409", description = "이미 존재하는 아이디"),
-            ApiResponse(responseCode = "422", description = "아이디 형식 오류"),
-            ApiResponse(responseCode = "422", description = "비밀번호 형식 오류"),
+            ApiResponse(
+                responseCode = "409",
+                description = "서버 상태 충돌",
+                content = [Content(
+                    mediaType = "application/json",
+                    examples = [ExampleObject(
+                        name = "AlreadyTakenUserId",
+                        summary = "아이디 이미 존재함",
+                        value = """
+                        { 
+                            "message": "아이디가 이미 존재합니다.",
+                            "body": null
+                        }
+                        """
+                    )]
+                )]
+            ),
+            ApiResponse(
+                responseCode = "422",
+                description = "데이터 형식 오류",
+                content = [Content(
+                    mediaType = "application/json",
+                    examples = [
+                        ExampleObject(
+                            name = "InvalidUserIdFormat-Length",
+                            summary = "아이디 형식 오류 - 길이",
+                            value = """
+                            { 
+                                "message": "아이디는 4자 이상 12자 이하여야 합니다.",
+                                "body": null
+                            }
+                            """
+                        ),
+                        ExampleObject(
+                            name = "InvalidUserIdFormat-SpecialCharacter",
+                            summary = "아이디 형식 오류 - 특수문자",
+                            value = """
+                            { 
+                                "message": "아이디는 영문 대소문자 및 숫자로만 구성되어야 합니다.",
+                                "body": null
+                            }
+                            """
+                        ),
+                        ExampleObject(
+                            name = "InvalidPasswordFormat-Length",
+                            summary = "비밀번호 형식 오류 - 길이",
+                            value = """
+                            { 
+                                "message": "비밀번호는 8자 이상 16자 이하여야 합니다.",
+                                "body": null
+                            }
+                            """
+                        ),
+                        ExampleObject(
+                            name = "InvalidPasswordFormat-SpecialCharacter",
+                            summary = "비밀번호 형식 오류 - 특수문자 포함",
+                            value = """
+                            { 
+                                "message": "비밀번호는 영문 대소문자, 숫자, 특수문자가 각각 한 개 이상씩 포함되어야 합니다.",
+                                "body": null
+                            }
+                            """
+                        ),
+                    ]
+                )]
+            ),
         ]
     )
     @PostMapping("/join")
-    fun join(@RequestBody request: JoinRequest): ResponseEntity<Map<String, String>> {
+    fun join(@RequestBody request: UserV1JoinRequest): ResponseEntity<ApiResponseV1<Any>> {
         // TODO: 구현 필요
-        return ResponseEntity.status(201).body(mapOf("message" to "회원 가입되었습니다."))
+        return ResponseEntity
+            .status(201)
+            .body(
+                ApiResponseV1(message = "회원 가입되었습니다."))
     }
 }
 
-data class JoinRequest(
-    val id: String,
-    val password: String
-) 
