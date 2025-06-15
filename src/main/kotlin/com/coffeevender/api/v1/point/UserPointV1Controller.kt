a0679d039a6eeceba5396d30cd3f297a3ca9fe3d
성승현 <ite476@gmail.com>
2025-06-15 17:35:56 +0900

    add: `POST /api/v1/point/charge` 반환 케이스 정의

--- src/main/kotlin/com/coffeevender/api/v1/point/UserPointV1Controller.kt
+++ src/main/kotlin/com/coffeevender/api/v1/point/UserPointV1Controller.kt
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.ExampleObject
 
 @RestController
 @RequestMapping("/api/v1/point")
@@ -15,10 +17,69 @@ class UserPointV1Controller {
         summary = "포인트 충전",
         description = "포인트를 충전하는 API",
         responses = [
-            ApiResponse(responseCode = "200", description = "성공"),
-            ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            ApiResponse(responseCode = "401", description = "인증 실패"),
-            ApiResponse(responseCode = "422", description = "포인트 충전 금액 부족"),
+            ApiResponse(
+                responseCode = "200", 
+                description = "성공",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        examples = [
+                            ExampleObject(
+                                name = "Success",
+                                summary = "성공",
+                                value = """
+                                { 
+                                    "message": "포인트 충전에 성공했습니다.",
+                                    "body": null
+                                }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "401", 
+                description = "인증 실패",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        examples = [
+                            ExampleObject(
+                                name = "AuthenticationFailed",
+                                summary = "인증 실패",
+                                value = """
+                                { 
+                                    "message": "회원 인증에 실패했습니다.",
+                                    "body": null
+                                }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "422", 
+                description = "포인트 충전 금액 부족",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        examples = [
+                            ExampleObject(
+                                name = "InsufficientPoint",
+                                summary = "포인트 충전 금액 부족",
+                                value = """
+                                { 
+                                    "message": "포인트 충전 금액은 0 이상의 정수여야 합니다.",
+                                    "body": null
+                                }
+                                """
+                            )
+                        ]
+                    )
+                ]
+            )
         ]
     )
     @PostMapping("/charge")
